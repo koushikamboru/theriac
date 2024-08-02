ceb869fc9fc4d96fc7a82fb4f1ae4988001e8bb8
gurramsubrahmanyam <gurramsubrahmanyam10@gmail.com>
2024-08-02 18:00:44 +0530

    anatamony

---
+++ anatomy/script.js
@@ -0,0 +1,22 @@
+const head = document.getElementById('head');
+const head1 = document.getElementById('head1');
+
+console.log(head);
+head.addEventListener("click",()=>{
+    head1.style.display= 'block'
+}
+    
+
+)
+
+
+// window.onload = function () {
+//     const pieces = document.getElementsByTagName('svg');
+//     for (var i = 0; pieces.length; i++) {
+//         let _piece = pieces[i];
+//         _piece.onclick = function(t) {
+//             if (t.target.getAttribute('data-position') != null) document.getElementById('data').innerHTML = t.target.getAttribute('data-position');
+//             if (t.target.parentElement.getAttribute('data-position') != null) document.getElementById('data').innerHTML = t.target.parentElement.getAttribute('data-position');
+//         }
+//     }
+// }
